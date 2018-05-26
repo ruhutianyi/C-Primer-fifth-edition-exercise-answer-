c0f10e6a2df67e7ee7090b89c917508a91c9065e
ruhutianyi <33476775+ruhutianyi@users.noreply.github.com>
2018-05-26 21:27:21 +0800

    Add files via upload

---
+++ 09/test.h
@@ -0,0 +1,15 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <list>
+#include <deque>
+#include <vector>
+#include <forward_list>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stack>
+
+using namespace std;
+
+#endif // TEST_H
