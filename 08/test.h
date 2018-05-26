859403089d4613ab75c5add5ab7a366af09ab292
ruhutianyi <33476775+ruhutianyi@users.noreply.github.com>
2018-05-26 21:26:22 +0800

    Add files via upload

---
+++ 08/test.h
@@ -0,0 +1,18 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//exercise 8.11 8.13
+struct PersonInfo {
+    string name;
+    vector<string> phones;
+};
+
+#endif // TEST_H
