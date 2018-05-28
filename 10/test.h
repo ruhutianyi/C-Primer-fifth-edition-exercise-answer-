3155b1ec7a6ca2427404fc632c71c32a0f09d235
ruhutianyi <33476775+ruhutianyi@users.noreply.github.com>
2018-05-28 20:45:14 +0800

    Add files via upload

---
+++ 10/test.h
@@ -0,0 +1,16 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <algorithm>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <list>
+#include <functional>
+#include <iterator>
+
+using namespace std;
+using namespace placeholders;
+
+#endif // TEST_H
